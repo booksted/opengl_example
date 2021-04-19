8fdf3e5464b02ac7a47c85e551b4da3ff32379e3
booksted <rublepritz79@gmail.com>
2021-04-19 20:12:22 +0900

    week_07
    - 카메라 설정값의 의미 이해
    
    - view transform 계산하기
    
    - 키보드 및 마우스로 카메라 조작하기
    
    - ImGui 라이브러리 설정 및 사용하기

--- src/context.h
+++ src/context.h
@@ -12,7 +12,12 @@ CLASS_PTR(Context)
 class Context {
 public:
     static ContextUPtr Create();
-    void Render();    
+    void Render();
+    void ProcessInput(GLFWwindow* window);
+    // 화면 크기 조정
+    void Reshape(int width, int height);
+    void MouseMove(double x, double y);
+    void MouseButton(int button, int action, double x, double y);
 private:
     Context() {}
     bool Init();
@@ -24,6 +29,23 @@ private:
     // 텍스쳐
     TextureUPtr m_texture;
     TextureUPtr m_texture2;
+
+    // clear color
+    glm::vec4 m_clearColor { glm::vec4(0.0f, 0.5f, 0.3f, 0.0f) };
+
+    // camera parameter
+    glm::vec3 m_cameraPos { glm::vec3(0.0f, 0.0f, 3.0f) };
+    glm::vec3 m_cameraFront { glm::vec3(0.0f, 0.0f, -1.0f) };
+    glm::vec3 m_cameraUp { glm::vec3(0.0f, 1.0f, 0.0f) };
+
+    bool m_cameraControl { false };
+    glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
+
+    int m_width { WINDOW_WIDTH };
+    int m_height { WINDOW_HEIGHT };
+
+    float m_cameraPitch { 0.0f };
+    float m_cameraYaw { 0.0f };
 };
 
 #endif // __CONTEXT_H__
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,18 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
     SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
-    glViewport(0, 0, width, height);
+    auto context = reinterpret_cast< Context* >(glfwGetWindowUserPointer(window));
+    context->Reshape(width, height);
 }
 
 void OnKeyEvent(GLFWwindow* window,
     int key, int scancode, int action, int mods) {
+    ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
     SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
         key, scancode,
         action == GLFW_PRESS ? "Pressed" :
@@ -23,6 +27,27 @@ void OnKeyEvent(GLFWwindow* window,
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
     }
+}
+
+void OnCursorPos(GLFWwindow* window, double x, double y) {
+    auto context = (Context*)glfwGetWindowUserPointer(window);
+    context->MouseMove(x, y);
+}
+
+void OnMouseButton(GLFWwindow* window, int button, int action, int modifier) {
+    ImGui_ImplGlfw_MouseButtonCallback(window, button, action, modifier);
+    auto context = (Context*)glfwGetWindowUserPointer(window);
+    double x, y;
+    glfwGetCursorPos(window, &x, &y);
+    context->MouseButton(button, action, x, y);
+}
+
+void OnCharEvent(GLFWwindow* window, unsigned int ch) {
+    ImGui_ImplGlfw_CharCallback(window, ch);
+}
+
+void OnScroll(GLFWwindow* window, double xoffset, double yoffset) {
+    ImGui_ImplGlfw_ScrollCallback(window, xoffset, yoffset);
 }
 
 int main(int argc, const char** argv) {
@@ -57,44 +82,72 @@ int main(int argc, const char** argv) {
     // opengl context 생성이후 실행
 
     // glad를 활용한 OpenGL 함수 로딩
-if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    SPDLOG_ERROR("failed to initialize glad");
-    glfwTerminate();
-    return -1;
-}
-auto glVersion = glGetString(GL_VERSION);
-SPDLOG_INFO("OpenGL context version: {}", glVersion);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version: {}", glVersion);
 
-auto context = Context::Create();
-if (!context) {
-    SPDLOG_ERROR("failed to create context");
-    glfwTerminate();
-    return -1;
-}
+    auto imguiContext = ImGui::CreateContext();
+    ImGui::SetCurrentContext(imguiContext);
+    ImGui_ImplGlfw_InitForOpenGL(window, false);
+    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplOpenGL3_CreateFontsTexture();
+    ImGui_ImplOpenGL3_CreateDeviceObjects();
 
-ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
-SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
-SPDLOG_INFO("fragment shader id: {}", fragShader->Get());
+    auto context = Context::Create();
+    if (!context) {
+        SPDLOG_ERROR("failed to create context");
+        glfwTerminate();
+        return -1;
+    }
 
-auto program = Program::Create({fragShader, vertShader});
-SPDLOG_INFO("program id: {}", program->Get());
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
+    SPDLOG_INFO("fragment shader id: {}", fragShader->Get());
 
-// 추가된 내용
-OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
-glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
-glfwSetKeyCallback(window, OnKeyEvent);
+    auto program = Program::Create({fragShader, vertShader});
+    SPDLOG_INFO("program id: {}", program->Get());
+
+    glfwSetWindowUserPointer(window, context.get());
+
+    // 추가된 내용
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
+    glfwSetCharCallback(window, OnCharEvent);
+    glfwSetCursorPosCallback(window, OnCursorPos);
+    glfwSetMouseButtonCallback(window, OnMouseButton);
+    glfwSetScrollCallback(window, OnScroll);
+
 
         // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
          // 화면 색상지정
+        context->ProcessInput(window);
         context->Render();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
         // front, back 뒤에먼저 그리고 앞으보이는 식 back->fornt 반복 // 더블 버퍼!
     }
     context.reset(); // context = nullptr; 메모리헤제 다른 방법
+
+    ImGui_ImplOpenGL3_DestroyFontsTexture();
+    ImGui_ImplOpenGL3_DestroyDeviceObjects();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext(imguiContext);
+
     glfwTerminate();
     
     return 0;
