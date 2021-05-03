cb040770a3f449d908e28761c5cd8d4f9db062d5
booksted <rublepritz79@gmail.com>
2021-05-03 18:59:26 +0900

    week_09
    빛에 따른 반사와 나무, 금속 재질에 따른 반사광 계산

--- src/context.h
+++ src/context.h
@@ -18,10 +18,12 @@ public:
     void Reshape(int width, int height);
     void MouseMove(double x, double y);
     void MouseButton(int button, int action, double x, double y);
+
 private:
     Context() {}
     bool Init();
     ProgramUPtr m_program;
+    ProgramUPtr m_simpleProgram;
 
     VertexLayoutUPtr m_vertexLayout;
     BufferUPtr m_vertexBuffer;
@@ -30,8 +32,28 @@ private:
     TextureUPtr m_texture;
     TextureUPtr m_texture2;
 
+    // animation
+    bool m_animation { true };
+
     // clear color
     glm::vec4 m_clearColor { glm::vec4(0.0f, 0.5f, 0.3f, 0.0f) };
+
+    // light parameter
+    struct Light {
+        glm::vec3 position { glm::vec3(3.0f, 3.0f, 3.0f) };
+        glm::vec3 ambient { glm::vec3(0.1f, 0.1f, 0.1f) };
+        glm::vec3 diffuse { glm::vec3(0.5f, 0.5f, 0.5f) };
+        glm::vec3 specular { glm::vec3(1.0f, 1.0f, 1.0f) };
+    };
+    Light m_light;
+
+    // material parameter
+    struct Material {
+        TextureUPtr diffuse;
+        TextureUPtr specular;
+        float shininess { 32.0f };
+    };
+    Material m_material;
 
     // camera parameter
     glm::vec3 m_cameraPos { glm::vec3(0.0f, 0.0f, 3.0f) };
